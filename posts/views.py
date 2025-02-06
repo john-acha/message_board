92ef3f78baa7f3e5cb9e71e85108cad81c14add7
john_acha <john_acha@live.com>
2025-02-05 20:44:12 -0500

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+# posts/views.py
+# from django.shortcuts import render
+
+# from .models import Post
+
+
+# # Create your views here.
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, "post_list.html", {"posts": posts})
+
+from django.views.generic import ListView  # new
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    template_name = "post_list.html"
